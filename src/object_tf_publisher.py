d4e0a39dcfeb64b90815230e3009b6abfd8a6d4e
Remco Kuijpers <remco.kuijpers@hotmail.nl>
2020-07-07 14:21:22 +0200

    Added objects to tf

---
+++ src/object_tf_publisher.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import rospy
+from gazebo_msgs.msg import ModelStates
+import tf
+
+class TF_broadcaster():
+    def __init__(self):
+        self.model = rospy.myargv()[2]
+        rospy.init_node(self.model + "_tf_publisher")
+        self.sub = rospy.Subscriber("/gazebo/model_states", ModelStates, self.callback, queue_size=10)
+        self.br = tf.TransformBroadcaster()
+        rospy.wait_for_message("/gazebo/model_states", ModelStates)
+
+    def callback(self, msg):
+        index = msg.name.index(self.model)
+        pose = msg.pose[index].position
+        rot = msg.pose[index].orientation
+        self.br.sendTransform((pose.x, pose.y, pose.z), (rot.x, rot.y, rot.z, rot.w), rospy.Time.now(), self.model, "map")
+
+if __name__ == "__main__":
+    TF = TF_broadcaster()
+    r = rospy.Rate(5)
+    while not rospy.is_shutdown():
+        r.sleep()
